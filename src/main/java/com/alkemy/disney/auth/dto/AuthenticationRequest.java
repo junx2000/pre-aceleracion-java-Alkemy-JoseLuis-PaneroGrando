7dfaba1cc891e974373db1734a5a9b2fedecee04
Jose Luis Panero Grando <57608370+junx2000@users.noreply.github.com>
2022-06-29 20:48:22 -0300

    Update AuthenticationRequest.java

--- src/main/java/com/alkemy/disney/auth/dto/AuthenticationRequest.java
+++ src/main/java/com/alkemy/disney/auth/dto/AuthenticationRequest.java
@@ -3,9 +3,13 @@ package com.alkemy.disney.auth.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class AuthenticationRequest {
+    @NotNull
     private String username;
+    @NotNull
     private String password;
 }
